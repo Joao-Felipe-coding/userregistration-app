e151b002a349d52bbf7a848bebb8bc993ba78fa8
Joao-Felipe-coding <jofelipe2018@gmail.com>
2026-04-09 19:55:26 -0300

    configurando os botões do report

--- app/src/main/java/com/joaofelipe/userregistrationapp/ReportActivity.java
+++ app/src/main/java/com/joaofelipe/userregistrationapp/ReportActivity.java
@@ -1,4 +1,26 @@
 package com.joaofelipe.userregistrationapp;
 
-public class ReportActivity {
+// Importações de componentes de UI, inteções e a biblioteca ROOM
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportActivity extends AppCompatActivity {
+    private TextView textViewReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Define o layout XML dessa tela de relatório
+        setContentView(R.layout.activity_report);
+        // Mapeamento do TextView do XML para o java
+        textViewReport = findViewById((R.id.textViewReport);
+        // Encontra o botão e define o clique para voltar
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        // O botão de retorno utilizando expressão lamdda
+        btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+    }
+
+
 }
